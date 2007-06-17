7c39cd43d560280f1287ecd39464b5a97856fe69
v.likic <v.likic@95beb699-422e-0410-a698-db1f1f78d2a9>
2007-06-17 21:40:46 +0000

    Added -t switch.

--- trunk/lib/seq_firstl.pl
+++ trunk/lib/seq_firstl.pl
@@ -6,19 +6,32 @@ use yabby_seq;
 use Getopt::Std;
 
 $USAGE = "
- Prints the first letter from the first sequence of a sequence
- object. 
+ Prints the first sequence letter from the sequence object. 
 
  Usage:
-        seq_firstl OBJ_NAME
+        seq_firstl [ options ] OBJ_NAME
+
+ where OBJ_NAME is the name of an existing sequence object.
+
+ Options:
+
+ -t -- print the last letter instead of the first
 
  Notes:
 
- 1. This merely an example script used to demonstrate how new
+ 1. This command is merely an example to demonstrate how new
  Yabby commands can be developed.
 ";
 
 # options
+getopts('t');
+
+if ( defined( $opt_t ) ) {
+  $opt_t_flag = 1;
+} else {
+  $opt_t_flag = 0;
+}
+
 # initialization
 @argl = sys_init( @ARGV );
 check_call( @argl, [ 1 ] );
@@ -30,7 +43,23 @@ $xmldoc = load_ip_xml( $obj_name, $SEQUENCE );
 
 # body
 $seq_hash = xml2seq( $xmldoc );
-$seq_item = $seq_hash->{1};
-printf " The first letter of the first sequence is: '%s'\n",
-  substr( $seq_item->{$DBA_SEQUENCE}, 0, 1 );
+$keys = get_seq_keys( $seq_hash );
+printf " '%s' contains %d sequence(s)\n", $obj_name, $#{$keys}+1;
+
+for $key ( @$keys ) {
+
+  $seq_item = $seq_hash->{$key};
+  $seq_id = $seq_item->{$DBA_SEQID};
+  $sequence = $seq_item->{$DBA_SEQUENCE};
+
+  if ( $opt_t_flag ) {
+    $lett = substr( $seq_item->{$DBA_SEQUENCE}, -1, 1 );
+  } else {
+    $lett = substr( $seq_item->{$DBA_SEQUENCE}, 0, 1 );
+  }
+
+  printf " Sequence '%s', the letter is '%s'\n", $seq_id, $lett;
+}
+
+
 
