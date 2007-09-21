d9e0cb489be99a6a2a21741762d05b546cafea5e
v.likic <v.likic@95beb699-422e-0410-a698-db1f1f78d2a9>
2007-09-21 09:16:13 +0000

    Created yabby_emboss.pm and moved routines for processing of needle output there.

--- lib/emboss_needle.pl
+++ lib/emboss_needle.pl
@@ -2,6 +2,7 @@
 
 use yabby_sys;
 use yabby_seq;
+use yabby_emboss;
 use yabby_utils;
 
 use Getopt::Std;
@@ -33,113 +34,12 @@ if (! unsig_int($nn) ) {
 # body
 print " Processing the file '$needle_file' ..\n\n";
 
-$needle_hash = {};
+$top_entries = proc_needle_output($needle_file, $nn);
 
-$fp = open_for_reading( $needle_file );
-
-$read_entry = 0;
-$entry_lines = [];
-$entry_no = -1;
-
-while ( <$fp> ) {
-
-  $record = $_;
-  #chomp( $record );
-  @fields = split " ", $record;
-
-  if ( defined($fields[1]) && ($fields[1] eq "Aligned_sequences:") ) {
-     if ( $read_entry == 1 ) {
-        print " Error 1!\n";
-        exit 0;
-     }
-     $read_entry = 1;
-     @$entry_lines = ();
-  }
-
-  if ( defined($fields[1]) && ($fields[1] eq "Score:") ) {
-     if ( $read_entry == 0 ) {
-        print " Error 0!\n";
-        exit 0;
-     }
-     $read_entry = 0;
-     $entry_pars = extract_entry( $entry_lines );
-     $entry_no++;
-     $needle_hash{$entry_no} = $entry_pars;
-  }
-
-  if ( $read_entry ) {
-     push @$entry_lines, $record;
-  }
-}
-
-close_file( $fp );
-
-# sort the hash by similarity
-@keyarray = ();
-for $key (sort { $needle_hash{$b}->[2] <=> $needle_hash{$a}->[2] }
-                keys %needle_hash) 
-{
-  push @keyarray, $key;
-}
-
-if ($nn > $#keyarray+1){
-  $k = $#keyarray+1;
-  error("there are only '$k' alignments ($nn requested)");
-}
-
-for $ii ( 0 .. $nn-1 ) {
-  $key = $keyarray[$ii];
-  $par = $needle_hash{$key};
+$ii = 0;
+for $par ( @$top_entries ) { 
+  $ii++;
   printf " (%d) %s:%s, Similarity: %.1f\n", $ii+1, $par->[0], $par->[1],
     $par->[2];
-}
-
-# subroutines
-
-sub extract_entry {
-
-    my ( $entry_lines ) = @_;
-    my ( @fields, $seq_name1, $seq_name2, $similarity );
-
-    # --- $entry_lines is a chunk like this:
-    # 1: G25M-82-MONOMER
-    # 2: LmjF01.0010
-    # Matrix: EBLOSUM62
-    # Gap_penalty: 10.0
-    # Extend_penalty: 0.5
-    #
-    # Length: 687
-    # Identity:      54/687 ( 7.9%)
-    # Similarity:    97/687 (14.1%)
-    # Gaps:         474/687 (69.0%)
-    # Score: 30.5
-
-    @fields = split " ", $entry_lines->[1];
-    $seq_name1 = $fields[2];
-    @fields = split " ", $entry_lines->[2];
-    $seq_name2 = $fields[2];
-    @fields = split " ", $entry_lines->[9];
-
-    if ($#fields == 4) {
-
-      $similarity = $fields[4];
-      substr( $similarity, -2 ) = "";
-
-    } elsif ($#fields == 3) {
-
-      $similarity = $fields[3];
-      substr( $similarity, 0, 1 ) = "";
-      substr( $similarity, -2 ) = "";
-
-    } else {
-
-      error("unexpected fields length")
-    }
-
-    $entry_pars = [];
-    push @$entry_pars, $seq_name1;
-    push @$entry_pars, $seq_name2;
-    push @$entry_pars, $similarity;
-    return $entry_pars;
 }
 
---
+++ lib/yabby_emboss.pm
@@ -0,0 +1,127 @@
+# yabby_emboss.pm
+
+$NEEDLE_PATH = "/usr/local/bin/needle";
+$TMP_FASTA_FILE = "/tmp/yabby.tmp";
+$NEEDLE_GAP_OPEN = 10.0;
+$NEEDLE_GAP_EXTEND = 0.5;
+
+sub proc_needle_output {
+
+  # $needle_file -- the needle file
+  # $nn -- the number of top entried to be returned
+  my ($needle_file, $nn) = @_;
+
+  my ($needle_hash, $fp, $read_entry, $entry_lines, $entry_no, $record);
+  my (@fields, $entry_pars, @keyarray, $key, $top_keys, $top_entries);
+
+  $needle_hash = {};
+
+  $fp = open_for_reading($needle_file);
+
+  $read_entry = 0;
+  $entry_lines = [];
+  $entry_no = -1;
+
+  while ( <$fp> ) {
+
+    $record = $_;
+    #chomp( $record );
+    @fields = split " ", $record;
+
+    if ( defined($fields[1]) && ($fields[1] eq "Aligned_sequences:") ) {
+      if ( $read_entry == 1 ) {
+        print " Error 1\n";
+        exit 0;
+      }
+      $read_entry = 1;
+      @$entry_lines = ();
+    }
+
+    if ( defined($fields[1]) && ($fields[1] eq "Score:") ) {
+      if ( $read_entry == 0 ) {
+        print " Error 0\n";
+        exit 0;
+      }
+      $read_entry = 0;
+      $entry_pars = extract_entry( $entry_lines );
+      $entry_no++;
+      $needle_hash{$entry_no} = $entry_pars;
+    }
+
+    if ( $read_entry ) {
+      push @$entry_lines, $record;
+    }
+  }
+
+  close_file($fp);
+
+  # sort the hash by similarity
+  @keyarray = ();
+  for $key (sort { $needle_hash{$b}->[2] <=> $needle_hash{$a}->[2] }
+      keys %needle_hash) {
+  push @keyarray, $key;
+  }
+
+  # $nn cannot be greater than the number of sequences in the database
+  if ($nn > $#keyarray+1 ) {
+    $nn = $#keyarray+1;
+  }
+
+  # take the top $nn hits
+  @top_keys = splice @keyarray, 0, $nn;
+  $top_entries = [];
+  for $key ( @top_keys ) {
+    push @$top_entries, $needle_hash{$key};
+  }
+
+  return $top_entries;
+}
+
+sub extract_entry {
+
+  my ($entry_lines) = @_;
+  my (@fields, $seq_name1, $seq_name2, $similarity, $entry_pars);
+
+  # --- $entry_lines is a chunk like this:
+  # 1: G25M-82-MONOMER
+  # 2: LmjF01.0010
+  # Matrix: EBLOSUM62
+  # Gap_penalty: 10.0
+  # Extend_penalty: 0.5
+  #
+  # Length: 687
+  # Identity:      54/687 ( 7.9%)
+  # Similarity:    97/687 (14.1%)
+  # Gaps:         474/687 (69.0%)
+  # Score: 30.5
+
+  @fields = split " ", $entry_lines->[1];
+  $seq_name1 = $fields[2];
+  @fields = split " ", $entry_lines->[2];
+  $seq_name2 = $fields[2];
+  @fields = split " ", $entry_lines->[9];
+
+  if ($#fields == 4) {
+
+    $similarity = $fields[4];
+    substr($similarity, -2) = "";
+
+  } elsif ($#fields == 3) {
+
+    $similarity = $fields[3];
+    substr($similarity, 0, 1) = "";
+    substr($similarity, -2) = "";
+
+  } else {
+    error("unexpected fields length")
+  }
+
+  $entry_pars = [];
+  push @$entry_pars, $seq_name1;
+  push @$entry_pars, $seq_name2;
+  push @$entry_pars, $similarity;
+  return $entry_pars;
+}
+
+return 1;
+
