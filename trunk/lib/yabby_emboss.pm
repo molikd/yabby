2228fd5c9992fc24acca354317b042cc618da77b
v.likic <v.likic@95beb699-422e-0410-a698-db1f1f78d2a9>
2011-05-23 06:54:38 +0000

    Added 'emboss_water'. At present exactly the same as 'emboss_needle'.

--- trunk/lib/yabby_emboss.pm
+++ trunk/lib/yabby_emboss.pm
@@ -11,6 +11,82 @@ $NEEDLE_GAP_OPEN = 10.0;
 $NEEDLE_GAP_EXTEND = 0.5;
 
 # returns $nn entries ordered by percent similarity as reported
+# by the EMBOSS program 'water'
+sub proc_water_output {
+
+  # $water_file -- the water output file
+  # $nn -- the number of top entries to be returned
+  my ($water_file, $nn) = @_;
+
+  my ($water_hash, $fp, $read_entry, $entry_lines, $entry_no, $record);
+  my (@fields, $entry_pars, @keyarray, $key, $top_keys, $top_entries);
+
+  $water_hash = {};
+
+  $fp = open_for_reading($water_file);
+
+  $read_entry = 0;
+  $entry_lines = [];
+  $entry_no = -1;
+
+  while ( <$fp> ) {
+
+    $record = $_;
+    #chomp( $record );
+    @fields = split " ", $record;
+
+    if ( defined($fields[1]) && ($fields[1] eq "Aligned_sequences:") ) {
+      if ( $read_entry == 1 ) {
+        print " Error 1\n";
+        exit 0;
+      }
+      $read_entry = 1;
+      @$entry_lines = ();
+    }
+
+    if ( defined($fields[1]) && ($fields[1] eq "Score:") ) {
+      if ( $read_entry == 0 ) {
+        print " Error 0\n";
+        exit 0;
+      }
+      $read_entry = 0;
+      $entry_pars = extract_entry($entry_lines);
+      $entry_no++;
+      $water_hash{$entry_no} = $entry_pars;
+    }
+
+    if ( $read_entry ) {
+      push @$entry_lines, $record;
+    }
+  }
+
+  close_file($fp);
+
+  # sort the hash by similarity
+  @keyarray = ();
+
+  for $key (sort { $water_hash{$b}->[2] <=> $water_hash{$a}->[2] }
+      keys %water_hash) {
+  push @keyarray, $key;
+  }
+
+  # $nn cannot be greater than the number of sequences in the database
+  if ($nn > $#keyarray+1 ) { $nn = $#keyarray+1; }
+
+  # take the top $nn hits
+  @top_keys = splice @keyarray, 0, $nn;
+
+  $top_entries = [];
+
+  for $key ( @top_keys ) {
+    push @$top_entries, $water_hash{$key};
+  }
+
+  return $top_entries;
+}
+
+
+# returns $nn entries ordered by percent similarity as reported
 # by 'needle'
 sub proc_needle_output {
 
